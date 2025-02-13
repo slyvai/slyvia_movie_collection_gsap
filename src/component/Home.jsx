85e18a96017434e2c304de4dcfc586e32a37161a
slyvia <slyviafams@gmail.com>
2025-02-13 19:24:03 +0700

    second-commit

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Swal from 'sweetalert2'
-import Icon from '../assets/icons8-user-24.png'
+import Banner from '../assets/avengers-infinity-war-banner-4k-4c-1920x1080.jpg'
 
 
 function Home() {
@@ -9,7 +9,7 @@ const [user, setUser] = useState([])
 const [search, setSearch] = useState('')
 const [filtered, setFiltered] = useState([])
 
-const getMovie = async (movieName) => {
+const getMovie = async () => {
   try {
     const response = await axios.get(`https://imdb.iamidiotareyoutoo.com/search?q=avengers`) 
   setUser(response.data.description)
@@ -57,10 +57,21 @@ useEffect(() => {
   return (
     <>
     <div className='container-title'>
-    <h1>Movie</h1> 
-    <input type='search' placeholder='Search' onChange={handleSearch} value={search} style={{width: '40%', height: '30px', textAlign: 'center'}}  /> <button onClick={filteredMovie} style={{height: '30px', borderRadius: '5px'}}>Search</button>
-    <img src={Icon} alt="" />
+    
+   
+    <div className='banner'>
+      <img src={Banner} alt="" />
+      <div className='overlay'>
+       <h1>Movie</h1> 
+        <div className='search-bar'>
+       <input type='search' placeholder='Search here...' onChange={handleSearch} value={search} style={{width: '40%', height: '30px'}}  /> <button onClick={filteredMovie}>Search</button>
+       </div>
+        <h2>Watch Avengers Series Here.</h2>
+        <p>The Avengers are a team of superheroes appearing in comic books published by Marvel Comics. Created by writer Stan Lee and artist Jack Kirby, they first appeared in "The Avengers" #1 in 1963. The original team consisted of Iron Man, Thor, Hulk, Ant-Man, and Wasp. Over the years, the roster has included numerous other characters such as Captain America, Black Widow, Hawkeye, Vision, Scarlet Witch, and many others</p>
+      </div>
     </div>
+    </div>
+
     
     
     <div className='movie-container'>
@@ -68,7 +79,7 @@ useEffect(() => {
       {filtered.map((movie) => {
           return (
             <div key={movie['#IMDB_ID']}>
-              <img style={{width: '70%', height: '70%'}} src={movie['#IMG_POSTER']} />
+              <img style={{width: '80%', height: '80%'}} src={movie['#IMG_POSTER']} />
               <div className='text-movie'>
               <h4>{movie['#AKA']}</h4>
               <p style={{fontSize: '12px', color: '#bbb'}}>{movie['#ACTORS']}</p>
